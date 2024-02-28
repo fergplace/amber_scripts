0846e7795e749369924b73a5cae427f1d582e7d5
fergplace <fplace@calstatela.edu>
2024-02-27 21:39:40 -0800

    Create change_radii_to_opt.py

---
+++ change_radii_to_opt.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from parmed.amber import AmberParm
+import sys
+
+def opt_radii(topology):
+    '''
+    This function is used to update atom radius based on opt standard
+    :param topology: path to the topology file
+    :return: replace the topology file with the new one
+    '''
+    parm = AmberParm(topology)
+    for i, atom in enumerate(parm.atoms):
+        if atom.atomic_number == 6: parm.atoms[i].solvent_radius = 1.4001	    # C
+        elif atom.atomic_number == 1: parm.atoms[i].solvent_radius = 1.5501      # H
+        elif atom.atomic_number == 7: parm.atoms[i].solvent_radius = 2.3501		# N
+        elif atom.atomic_number == 8: parm.atoms[i].solvent_radius = 1.2801		# O
+        elif atom.atomic_number == 16: parm.atoms[i].solvent_radius = 1.8000		# S
+
+    for i, atom in enumerate(parm.atoms):
+        if atom.atomic_number == 1:
+            parm.atoms[i].screen = 0.85
+        elif atom.atomic_number == 6:
+            parm.atoms[i].screen = 0.72
+        elif atom.atomic_number == 7:
+            parm.atoms[i].screen = 0.79
+        elif atom.atomic_number == 8:
+            parm.atoms[i].screen = 0.85
+        elif atom.atomic_number == 9:
+            parm.atoms[i].screen = 0.88
+        elif atom.atomic_number == 15:
+            parm.atoms[i].screen = 0.86
+        elif atom.atomic_number == 16:
+            parm.atoms[i].screen = 0.96
+        else:
+            parm.atoms[i].screen = 0.8
+
+    new_topology = topology.split('.')[-2]
+    new_name = new_topology + '_opt_radii'
+
+    parm.write_parm(topology.replace(new_topology, new_name))
+
+if __name__ == '__main__':
+    topology = sys.argv[1]
+    opt_radii(topology)
