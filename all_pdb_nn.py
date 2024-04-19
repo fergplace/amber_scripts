1c96ce1daa9cf6ea76c3cea2ecdcf07b7dc09e39
fergplace <fplace@calstatela.edu>
2024-04-19 09:50:39 -0700

    Create all_pdb_nn.py

---
+++ all_pdb_nn.py
@@ -0,0 +1,15 @@
+import glob
+import numpy as np 
+import pandas as pd
+import os 
+def main()   :
+    print("working")
+    tmp_file_loc = "*_dir/6m0j_noHet_*.pdb"
+    tmp_file_names = glob.glob(tmp_file_loc)
+    dir_path = "all_pdb_nn"
+    os.system(f"mkdir {dir_path}")
+    
+    for file in tmp_file_names :
+        cp_path = dir_path + "/" + file
+        os.system(f"cp {file} {cp_path}")
+    
