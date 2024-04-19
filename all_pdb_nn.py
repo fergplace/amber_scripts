4f26128c4c1b26696f9bbdfe8e31259e1cb6d13a
fergplace <fplace@calstatela.edu>
2024-04-19 09:55:30 -0700

    Update all_pdb_nn.py

--- all_pdb_nn.py
+++ all_pdb_nn.py
@@ -5,14 +5,17 @@ import os
 
 def main()   :
     print("working")
-    tmp_file_loc = "*_dir/6m0j_noHet_*.pdb"
+    tmp_file_loc = "*_dir/6m0j_noHet_*A.pdb"
     tmp_file_names = glob.glob(tmp_file_loc)
     dir_path = "all_pdb_nn"
     os.system(f"mkdir {dir_path}")
     
     for file in tmp_file_names :
+        print(file)
         cp_path = dir_path + "/" + file
         os.system(f"cp {file} {cp_path}")
+        
+        break 
 
 if __name__ == '__main__':
     main()
