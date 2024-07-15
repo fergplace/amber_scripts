60e6a4babe9f6cbe82c382fbde9297a9e0b73633
fergplace <fplace@calstatela.edu>
2024-07-15 12:20:09 -0700

    Update sim_setup.py

--- testing_sim_pipeline/sim_setup.py
+++ testing_sim_pipeline/sim_setup.py
@@ -127,32 +127,31 @@ def change_radii_sh( pdbfh_base_name, cwd  ) :
 def equil_sh_gen():
 
 	equil_sh = ['''#!/bin/bash
-	#SBATCH --job-name=run_equil
-	#SBATCH --partition=gpu
-	#SBATCH --gpus=4
-	#SBATCH --cpus-per-gpu=12
-	#SBATCH --output=run_equil.out
-	#SBATCH --error=run_equil.error
-	#SBATCH --time=48:00:00
+#SBATCH --job-name=run_equil
+#SBATCH --partition=gpu
+#SBATCH --gpus=4
+#SBATCH --cpus-per-gpu=12
+#SBATCH --output=run_equil.out
+#SBATCH --error=run_equil.error
+#SBATCH --time=48:00:00
 
-	echo "Loading modules..."
-	module load amber  
-	source /opt/calstatela/amber-22/amber22/amber.sh
+echo "Loading modules..."
+module load amber  
+source /opt/calstatela/amber-22/amber22/amber.sh
 
-	$AMBERHOME/bin/pmemd.cuda -O -i min.in -o min.out -p 6m0j_noHet_solvated.prmtop -c 6m0j_noHet_solvated.inpcrd \
-	-r min.rst 
-	echo "min done"
-	$AMBERHOME/bin/pmemd.cuda -O -i heat.in -o heat.out -p 6m0j_noHet_solvated.prmtop -c min.rst \
-	-r heat.rst -x heat.mdcrd -ref min.rst
+$AMBERHOME/bin/pmemd.cuda -O -i min.in -o min.out -p 6m0j_noHet_solvated.prmtop -c 6m0j_noHet_solvated.inpcrd \
+-r min.rst 
+echo "min done"
+$AMBERHOME/bin/pmemd.cuda -O -i heat.in -o heat.out -p 6m0j_noHet_solvated.prmtop -c min.rst \
+-r heat.rst -x heat.mdcrd -ref min.rst
 
-	echo "heat done"
-	$AMBERHOME/bin/pmemd.cuda -O -i density.in -o density.out -p 6m0j_noHet_solvated.BOX.prmtop -c heat.rst \
-	-r density.rst -x density.mdcrd -ref heat.rst
+echo "heat done"
+$AMBERHOME/bin/pmemd.cuda -O -i density.in -o density.out -p 6m0j_noHet_solvated.BOX.prmtop -c heat.rst \
+-r density.rst -x density.mdcrd -ref heat.rst
 
-	echo "density done"
-	$AMBERHOME/bin/pmemd.cuda -O -i equil.in -o equil.out -p 6m0j_noHet_solvated.prmtop -c density.rst \
-	-r equil.rst -x equil.mdcrd
-	''']
+echo "density done"
+$AMBERHOME/bin/pmemd.cuda -O -i equil.in -o equil.out -p 6m0j_noHet_solvated.prmtop -c density.rst \
+-r equil.rst -x equil.mdcrd''']
 
 	with open("equil.sh", "w+") as equil_sh_file : 
 		for line in equil_sh : 
@@ -164,35 +163,34 @@ def equil_sh_gen():
 def run_sim_sh_gen():
 
 	sim_sh = ['''#!/bin/bash
-	#SBATCH --job-name=run_prod
-	#SBATCH --partition=gpu
-	#SBATCH --ntasks=4
-	#SBATCH --cpus-per-task=12
-	#SBATCH --gpus-per-task=1
-	#SBATCH --output=run_prod.out
-	#SBATCH --error=run_prod.error
-	#SBATCH --time=48:00:00
+#SBATCH --job-name=run_prod
+#SBATCH --partition=gpu
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=12
+#SBATCH --gpus-per-task=1
+#SBATCH --output=run_prod.out
+#SBATCH --error=run_prod.error
+#SBATCH --time=48:00:00
 
-	echo "Loading modules..."
-	module load amber  
-	source /opt/calstatela/amber-22/amber22/amber.sh
+echo "Loading modules..."
+module load amber  
+source /opt/calstatela/amber-22/amber22/amber.sh
 
 
-	mpirun -v -np 4 $AMBERHOME/bin/pmemd.cuda.MPI -O -i prod.in -o prod1.out \
-	-p 6m0j_noHet_solvated.prmtop -c equil.rst -r prod1.rst -x prod1.mdcrd
+mpirun -v -np 4 $AMBERHOME/bin/pmemd.cuda.MPI -O -i prod.in -o prod1.out \
+-p 6m0j_noHet_solvated.prmtop -c equil.rst -r prod1.rst -x prod1.mdcrd
 
-	echo "prod 1 done ..."
-	mpirun -v -np 4 $AMBERHOME/bin/pmemd.cuda.MPI -O -i prod.in -o prod2.out \
-	-p 6m0j_noHet_solvated.prmtop -c prod1.rst -r prod2.rst -x prod2.mdcrd
+echo "prod 1 done ..."
+mpirun -v -np 4 $AMBERHOME/bin/pmemd.cuda.MPI -O -i prod.in -o prod2.out \
+-p 6m0j_noHet_solvated.prmtop -c prod1.rst -r prod2.rst -x prod2.mdcrd
 
-	echo "prod 2 done..."
-	mpirun -v -np 4 $AMBERHOME/bin/pmemd.cuda.MPI -O -i prod.in -o prod3.out \
-	-p 06m0j_noHet_solvated.prmtop -c prod2.rst -r prod3.rst -x prod3.mdcrd
+echo "prod 2 done..."
+mpirun -v -np 4 $AMBERHOME/bin/pmemd.cuda.MPI -O -i prod.in -o prod3.out \
+-p 06m0j_noHet_solvated.prmtop -c prod2.rst -r prod3.rst -x prod3.mdcrd
 
-	echo "prod 3 done..."
-	mpirun -v -np 4 $AMBERHOME/bin/pmemd.cuda.MPI -O -i prod.in -o prod4.out \
-	-p 6m0j_noHet_solvated.prmtop -c prod3.rst -r prod4.rst -x prod4.mdcrd
-	''']
+echo "prod 3 done..."
+mpirun -v -np 4 $AMBERHOME/bin/pmemd.cuda.MPI -O -i prod.in -o prod4.out \
+-p 6m0j_noHet_solvated.prmtop -c prod3.rst -r prod4.rst -x prod4.mdcrd''']
 
 	with open("run_sim.sh", "w+") as sim_sh_file : 
 		for line in sim_sh : 
